b35c15d483263c501dcba866f9911df184440905
Harsh Singh <baghel.harsh@webseeder.tech>
2025-06-25 13:52:20 +0530

    fix bugs

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer.jsx';
 import Home from '../Products/Home.jsx';
-import Products from '../Products/products.jsx';
+import Products from '../Products/Products.jsx';
 import MobileBottomNav from '../components/MobileBottomNav';
 import ScrollToTopButton from '../components/ScrollToTopButton';
 
