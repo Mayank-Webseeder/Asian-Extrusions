f34de3e599cf23f68a00ef58eee310c9cb3b6192
Harsh Singh <baghel.harsh@webseeder.tech>
2025-06-25 17:33:33 +0530

    ix background issue

--- src/About/MissionVision.jsx
+++ src/About/MissionVision.jsx
@@ -7,9 +7,9 @@ const MissionVision = () => {
       {/* Left: Mission */}
       <div className="relative w-full md:w-1/2 text-white">
         {/* Background image */}
-        <div className="absolute inset-0 bg-[url('About/about6.avif')] bg-cover bg-center" />
+        <div className="absolute inset-0 bg-[url('/About/about6.avif')] bg-cover bg-center" />
         {/* Blue overlay */}
-        <div className="absolute inset-0 bg-[#045184]/85" />
+        <div className="absolute inset-0 bg-[#045184]/85 bg-opacity-80" />
 
         {/* Content */}
         <div className="relative z-10 p-10 md:p-20 max-w-md mx-auto text-center space-y-6">
